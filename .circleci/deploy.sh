ce73e4c9b13aa283cc0395a12ccd0bcaa25ac341
Diego Pedroso <diego.pedroso@dott.pt>
2021-05-28 11:52:10 -0300

    test v3

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -56,17 +56,17 @@ FULL_DOCKER_IMAGE_NAME=$(cat workspace/full_docker_image_name)
 KUBE_CONFIG=$(cat APP_NAME/manifests/helloweb-all-in-one.yaml.template | sed "s|DOCKER_IMAGE_NAME|$FULL_DOCKER_IMAGE_NAME|g")
 echo "$KUBE_CONFIG" | kubectl apply -f -
 # Wait for deployment to finish
-kubectl rollout status deployment/helloweb
+kubectl rollout status deployment/APP_NAME
 kubectl get pods
 
 # Wait for external ip to be assigned
 sleep 60
-kubectl get service helloweb
-EXTERNAL_IP=$(kubectl get service helloweb -o json | jq -r ".status.loadBalancer.ingress[0].ip")
+kubectl get service APP_NAME
+EXTERNAL_IP=$(kubectl get service APP_NAME -o json | jq -r ".status.loadBalancer.ingress[0].ip")
 curl "http://$EXTERNAL_IP"
 
 if [ "${DELETE_CLUSTER_AT_END_OF_TEST}" == "true" ]
 then
-kubectl delete service helloweb
+kubectl delete service APP_NAME
 gcloud --quiet container clusters delete $GOOGLE_CLUSTER_NAME
 fi

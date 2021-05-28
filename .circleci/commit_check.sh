b38d803fae30a942ebc4786869bc3160907be3b7
Diego Pedroso <diego.pedroso@dott.pt>
2021-05-28 10:21:59 -0300

    test diff between paths

---
+++ .circleci/commit_check.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+# latest commit
+LATEST_COMMIT=$(git rev-parse HEAD)
+
+# latest commit where path/to/folder1 was changed
+FOLDER1_COMMIT=$(git log -1 --format=format:%H --full-diff hello-app-v1/)
+
+# latest commit where path/to/folder2 was changed
+FOLDER2_COMMIT=$(git log -1 --format=format:%H --full-diff hello-app-v2/)
+
+if [ $FOLDER1_COMMIT = $LATEST_COMMIT ];
+    then
+        echo "hello-app-v1 has changed"
+        sed -i "s/APP_NAME/hello-app-v1/g" .circleci/deploy.yaml       
+        .circleci/deploy.yaml
+elif [ $FOLDER2_COMMIT = $LATEST_COMMIT ];
+    then
+        echo "hello-app-v2 has changed"
+        sed -i "s/APP_NAME/hello-app-v2/g" .circleci/deploy.yaml       
+        .circleci/deploy.yaml
+else
+     echo "no folders of relevance has changed"
+     circleci-agent step halt
+fi

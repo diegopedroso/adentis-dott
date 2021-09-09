533d74b87d353035e25ea9c01bb4783c37dce634
Diego Pedroso <diego.pedroso@dott.pt>
2021-09-09 10:34:02 -0300

    tag 1

---
+++ .circleci/common.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+gke_credentials() {
+  env_vars
+  if [[ "$ENV" == "prod" ]]; then
+    echo "export GKE_ACCESS_KEY_ID=$GKE_ACCESS_KEY_ID_PROD" >> $BASH_ENV
+    echo "export GKE_SECRET_ACCESS_KEY=$GKE_SECRET_ACCESS_KEY_PROD" >> $BASH_ENV
+    echo "export GKE_CLUSTER_NAME=$PROD_CLUSTER_NAME" >> $BASH_ENV
+    echo "export GKE_REGION=$PROD_GKE_REGION" >> $BASH_ENV
+  elif [[ "$ENV" == "stg" ]]; then
+    echo "export GKE_ACCESS_KEY_ID=$GKE_ACCESS_KEY_ID_STG" >> $BASH_ENV
+    echo "export GKE_SECRET_ACCESS_KEY=$GKE_SECRET_ACCESS_KEY_STG" >> $BASH_ENV
+    echo "export GKE_CLUSTER_NAME=$STG_CLUSTER_NAME" >> $BASH_ENV
+    echo "export GKE_REGION=$STG_GKE_REGION" >> $BASH_ENV
+  elif [[ "$ENV" == "qa" ]]; then
+    echo "export GKE_ACCESS_KEY_ID=$GKE_ACCESS_KEY_ID_QA" >> $BASH_ENV
+    echo "export GKE_SECRET_ACCESS_KEY=$GKE_SECRET_ACCESS_KEY_QA" >> $BASH_ENV
+    echo "export GKE_CLUSTER_NAME=$QA_CLUSTER_NAME" >> $BASH_ENV
+    echo "export GKE_REGION=$QA_GKE_REGION" >> $BASH_ENV
+  fi
+  source $BASH_ENV
+}
+
+slack_webhook() {
+  env_vars
+  if [[ "$ENV" == "prod" ]]; then
+    echo "export SLACK_WEBHOOK=$SLACK_WEBHOOK_PROD" >> $BASH_ENV && source $BASH_ENV
+  elif [[ "$ENV" == "stg" ]]; then
+    echo "export SLACK_WEBHOOK=$SLACK_WEBHOOK_STG" >> $BASH_ENV && source $BASH_ENV
+  elif [[ "$ENV" == "qa" ]]; then
+    echo "export SLACK_WEBHOOK=$SLACK_WEBHOOK_QA" >> $BASH_ENV && source $BASH_ENV
+  fi
+}
---
+++ .circleci/run_test.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+set -xe
+
+echo '
+yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
+yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
+yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
+yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
+yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
+yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
+yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
+yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
+yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
+yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
+yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
+yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
+yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
+yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
+yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
+yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
+yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyysyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyysyyyyyyyyyyssyyyyyyyyyyyyyyyyyyy
+yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyo+:.+yyyyyyyyyyyyyyyyyyyyyyyyyyyso/-`syyyyyso/-`+yyyyyyyyyyyyyyyyyyy
+yyyyyyyyyyyyyyyyyyyyyyyyyyyyyys.`   /yyyyyyyyyyyyyyyyyyyyyyyyyy+``   oyyyyo.`   +yyyyyyyyyyyyyyyyyyy
+yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyo     /yyyyyyyyyyyyyyyyyyyyyyyyyy/     oyyyy+     +yyyyyyyyyyyyyyyyyyy
+yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyo     /yyyyyyyyyyyyyyyyyyyyyyssss/     ossss+     +ssssyyyyyyyyyyyyyyy
+yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyo     /yyyyyyyyyyyyyyyyyyyyyo....`     ......     `....oyyyyyyyyyyyyyy
+yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyo     /yyyyyyyyyyyyyyyyyyyyy+                          oyyyyyyyyyyyyyy
+yyyyyyyyyyyyyyyyyyyyyyyssssssyo     /yyyyyyyyysssosssyyyyy+`````     ``````     `````oyyyyyyyyyyyyyy
+yyyyyyyyyyyyyyyyyyys/-``    `.-     /yyyyyyo:.`     `-/oyyyyyyy/     oyyyy+     +yyyyyyyyyyyyyyyyyyy
+yyyyyyyyyyyyyyyyys-                 /yyyy+.             .oyyyyy/     oyyyy+     +yyyyyyyyyyyyyyyyyyy
+yyyyyyyyyyyyyyyy+       .--`        /yyy-      `.-.`      :yyyy/     oyyyy+     +yyyyyyyyyyyyyyyyyyy
+yyyyyyyyyyyyyyy+     .+yyyyys:      /yy-     -oyyyyyo.     /yyy/     oyyyy+     +yyyyyyyyyyyyyyyyyyy
+yyyyyyyyyyyyyyy.    `syyyyyyyy/     /ys     -yyyyyyyys.     yyy/     oyyyy+     +yyyyyyyyyyyyyyyyyyy
+yyyyyyyyyyyyyyy`    -yyyyyyyyyo     /yo     +yyyyyyyyy:     syy/     oyyyy+     +yyyyyyyyyyyyyyyyyyy
+yyyyyyyyyyyyyyy-    `oyyyyyyys-     oys`    .syyyyyyys.    `yyy/     oyyyy+     +yyyyyyyyyyyyyyyyyyy
+yyyyyyyyyyyyyyyo`    `:+ssso/.     :yyy/     ./ossso/`     +yyy/     oyyyy+     +yyyyyyyyyyyyyyyyyyy
+yyyyyyyyyyyyyyyyo.      ````      :syyyy/`     `````     `+yyyy/     oyyyy+     +yyyyyyyyyyyyyyyyyyy
+yyyyyyyyyyyyyyyyys/.           `-oyyyyyyyo:`           `:syyyyy/  ``-syyyy+   `-oyyyyyyyyyyyyyyyyyyy
+yyyyyyyyyyyyyyyyyyys+:-.````.:/oyyyyyyyyyyys+:..```.-:+syyyyyyy+-/osyyyyyyo./osyyyyyyyyyyyyyyyyyyyyy
+yyyyyyyyyyyyyyyyyyyyyyyysssyyyyyyyyyyyyyyyyyyyyyssyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
+yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
+yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
+yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
+yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
+yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
+yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
+yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
+yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
+yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
+yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
+yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
+yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
+yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
+yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
+yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
+syyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyys'

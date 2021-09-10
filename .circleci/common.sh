986b9505078e3b51b2f85dff0ccf43d1f60cba97
Diego Pedroso <diego.pedroso@dott.pt>
2021-09-10 14:26:06 -0300

    tag v1

--- .circleci/common.sh
+++ .circleci/common.sh
@@ -10,20 +10,14 @@ env_vars() {
 gke_credentials() {
   env_vars
   if [[ "$ENV" == "prod" ]]; then
-    echo "export GKE_ACCESS_KEY_ID=$GKE_ACCESS_KEY_ID_PROD" >> $BASH_ENV
-    echo "export GKE_SECRET_ACCESS_KEY=$GKE_SECRET_ACCESS_KEY_PROD" >> $BASH_ENV
-    echo "export GKE_CLUSTER_NAME=$PROD_CLUSTER_NAME" >> $BASH_ENV
+    echo "export GKE_ACCESS_KEY_ID=$GCLOUD_SERVICE_KEY" >> $BASH_ENV
+    echo "export GKE_CLUSTER_NAME=$GOOGLE_CLUSTER_NAME" >> $BASH_ENV
     echo "export GKE_REGION=$PROD_GKE_REGION" >> $BASH_ENV
   elif [[ "$ENV" == "stg" ]]; then
-    echo "export GKE_ACCESS_KEY_ID=$GKE_ACCESS_KEY_ID_STG" >> $BASH_ENV
-    echo "export GKE_SECRET_ACCESS_KEY=$GKE_SECRET_ACCESS_KEY_STG" >> $BASH_ENV
-    echo "export GKE_CLUSTER_NAME=$STG_CLUSTER_NAME" >> $BASH_ENV
-    echo "export GKE_REGION=$STG_GKE_REGION" >> $BASH_ENV
-  elif [[ "$ENV" == "qa" ]]; then
-    echo "export GKE_ACCESS_KEY_ID=$GKE_ACCESS_KEY_ID_QA" >> $BASH_ENV
-    echo "export GKE_SECRET_ACCESS_KEY=$GKE_SECRET_ACCESS_KEY_QA" >> $BASH_ENV
-    echo "export GKE_CLUSTER_NAME=$QA_CLUSTER_NAME" >> $BASH_ENV
-    echo "export GKE_REGION=$QA_GKE_REGION" >> $BASH_ENV
+    echo "export GKE_ACCESS_KEY_ID=$GCLOUD_SERVICE_KEY" >> $BASH_ENV
+    echo "export GKE_CLUSTER_NAME=$GOOGLE_CLUSTER_NAME" >> $BASH_ENV
+    echo "export GOOGLE_PROJECT_ID=$GOOGLE_PROJECT_ID" >> $BASH_ENV
+    echo "export GKE_REGION=$GOOGLE_COMPUTE_ZONE" >> $BASH_ENV
   fi
   source $BASH_ENV
 }
@@ -34,7 +28,5 @@ slack_webhook() {
     echo "export SLACK_WEBHOOK=$SLACK_WEBHOOK_PROD" >> $BASH_ENV && source $BASH_ENV
   elif [[ "$ENV" == "stg" ]]; then
     echo "export SLACK_WEBHOOK=$SLACK_WEBHOOK_STG" >> $BASH_ENV && source $BASH_ENV
-  elif [[ "$ENV" == "qa" ]]; then
-    echo "export SLACK_WEBHOOK=$SLACK_WEBHOOK_QA" >> $BASH_ENV && source $BASH_ENV
   fi
 }
--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -9,35 +9,21 @@ curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyr
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates gnupg jq kubectl google-cloud-sdk
 
-if [ "${CIRCLE_BRANCH}" == "main" ]
-then
-DOCKER_IMAGE_TAG=v${CIRCLE_BUILD_NUM}
-echo "$DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG" > full_docker_image_name
-else
-DOCKER_IMAGE_TAG=${CIRCLE_BRANCH}
-echo "APP_NAME:$DOCKER_IMAGE_TAG" > full_docker_image_name
-fi
-FULL_DOCKER_IMAGE_NAME=$(cat full_docker_image_name)
-docker build -t eu.gcr.io/$GOOGLE_PROJECT_ID/$FULL_DOCKER_IMAGE_NAME -f APP_NAME/Dockerfile APP_NAME/
 
-if [ "${CIRCLE_BRANCH}" == "main" ]
-then
-FULL_DOCKER_IMAGE_NAME=$(cat full_docker_image_name)
+DOCKER_IMAGE_TAG=v$TAG
+echo "$$APP:$DOCKER_IMAGE_TAG" > full_$APP
+FULL_$APP=$(cat full_$APP)
+docker build -t eu.gcr.io/$GOOGLE_PROJECT_ID/$FULL_$APP -f $APP/Dockerfile $APP/
+
+FULL_$APP=$(cat full_$APP)
 echo $GCLOUD_SERVICE_KEY | base64 --decode --ignore-garbage > gcloud-service-key.json
 gcloud auth activate-service-account --key-file gcloud-service-key.json
 gcloud --quiet auth configure-docker
-docker push eu.gcr.io/$GOOGLE_PROJECT_ID/$FULL_DOCKER_IMAGE_NAME
-else
-echo "Not main branch; skipping image push.."
-fi
+docker push eu.gcr.io/$GOOGLE_PROJECT_ID/$FULL_$APP
 
-FULL_DOCKER_IMAGE_NAME=$(cat full_docker_image_name)
-docker run -d --rm -p 8080:8080 --name APP_NAME eu.gcr.io/$GOOGLE_PROJECT_ID/$FULL_DOCKER_IMAGE_NAME
-docker run --network container:APP_NAME appropriate/curl --retry 10 --retry-connrefused http://localhost:8080
-if [ "${CIRCLE_BRANCH}" != "main" ]
-then
-circleci step halt
-fi
+FULL_$APP=$(cat full_$APP)
+docker run -d --rm -p 8080:8080 --name $APP eu.gcr.io/$GOOGLE_PROJECT_ID/$FULL_$APP
+docker run --network container:$APP appropriate/curl --retry 10 --retry-connrefused http://localhost:8080
 
 echo $GCLOUD_SERVICE_KEY | base64 --decode --ignore-garbage > gcloud-service-key.json
 set -x
@@ -46,16 +32,16 @@ gcloud --quiet config set project $GOOGLE_PROJECT_ID
 gcloud --quiet config set compute/zone $GOOGLE_COMPUTE_ZONE
 gcloud --quiet container clusters get-credentials $GOOGLE_CLUSTER_NAME
 
-FULL_DOCKER_IMAGE_NAME=$(cat full_docker_image_name)
-# Replace DOCKER_IMAGE_NAME placeholder in manifest with actual image name
-KUBE_CONFIG=$(cat APP_NAME/manifests/helloweb-all-in-one.yaml | sed "s|DOCKER_IMAGE_NAME|eu.gcr.io/$GOOGLE_PROJECT_ID/$FULL_DOCKER_IMAGE_NAME|g")
+FULL_$APP=$(cat full_$APP)
+# Replace $APP placeholder in manifest with actual image name
+KUBE_CONFIG=$(cat apps/$APP/kubernetes/$ENV-values.yaml | sed "s|$APP|eu.gcr.io/$GOOGLE_PROJECT_ID/$FULL_$APP|g")
 echo "$KUBE_CONFIG" | kubectl apply -f -
 # Wait for deployment to finish
-kubectl rollout status deployment/APP_NAME -n APP_NAME
-kubectl get pods -n APP_NAME
+kubectl rollout status deployment/$APP -n $APP
+kubectl get pods -n $APP
 
 # Wait for external ip to be assigned
 sleep 60
-kubectl get service APP_NAME -n APP_NAME
-EXTERNAL_IP=$(kubectl get service APP_NAME -n APP_NAME -o json | jq -r ".status.loadBalancer.ingress[0].ip")
+kubectl get service $APP -n $APP
+EXTERNAL_IP=$(kubectl get service $APP -n $APP -o json | jq -r ".status.loadBalancer.ingress[0].ip")
 curl "http://$EXTERNAL_IP"

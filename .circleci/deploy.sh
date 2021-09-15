75de12ad9d6d6e0fe7db8e68f8cb4a58bea8a438
Diego Pedroso <diego.pedroso@dott.pt>
2021-09-15 10:30:48 -0300

    test service integration

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -44,5 +44,5 @@ kubectl get pods -n $APP
 # Wait for external ip to be assigned
 sleep 30
 kubectl get service $APP -n $APP
-EXTERNAL_IP=$(kubectl get service $APP -n $APP -o json | jq -r ".status.loadBalancer.ingress[0].ip")
-curl "http://$EXTERNAL_IP"
+# EXTERNAL_IP=$(kubectl get service $APP -n $APP -o json | jq -r ".status.loadBalancer.ingress[0].ip")
+# curl "http://$EXTERNAL_IP"
